ba3628e552e2eefdc204c62507b286a4b2c89716
souleyman-NSM <yacef_s@etna-alternance.net>
2025-01-29 16:36:37 +0100

    qsd

--- components/core/Navbar.js
+++ components/core/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
         </li>
       </ul>
       <a
-        href="https://github.com/jayhawk24/jayhawk24.github.io/"
+        href="https://github.com/souleyman-NSM/"
         className="github-corner"
         aria-label="View source on GitHub"
       >
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
+      hello
       <Main />
     </Layout>
   );
