6e6a07fe99956c7c36659ed889dd4b0186d50b64
souleyman-NSM <yacef_s@etna-alternance.net>
2025-01-30 13:46:33 +0100

    package

--- data.js
+++ data.js
@@ -10,14 +10,14 @@ export const home = {
 
 export const socials = {
     // Only usernames.
-    twitter: "jayh4wk",
-    github: "jayhawk24",
-    instagram: "__jayhawk__"
+    twitter: "",
+    github: "",
+    instagram: ""
 };
 
 export const about = {
     aboutMe:
-        "Salem wa aleikoum, I am Souleyman Gupta from Varanasi, India I am a Full Stack Developer at Gigwalla. Apart from coding, there are a lot of activities I love to do like...",
+        "Salem wa aleikoum, I am Souleyman",
     hobbies: ["Basketball", "Chess", "Hiking", "Swimming"],
     quote: "Write code that changes Lives...",
     skills: [
